d006108f7956c482f9412a0968dac81c3735c4ae
carlson5uiuc <carlson5uiuc@a0ffda31-7524-4b21-e595-ff3db66bd2c9>
2010-11-17 07:25:59 +0000

    git-svn-id: https://boomshakalaka.googlecode.com/svn/trunk@59 a0ffda31-7524-4b21-e595-ff3db66bd2c9

---
+++ CS598rhk/src/edu/illinois/CS598rhk/schedules/AlwaysSchedule.java
@@ -0,0 +1,25 @@
+package edu.illinois.CS598rhk.schedules;
+
+public class AlwaysSchedule extends DiscoverSchedule {
+
+    public AlwaysSchedule() {
+        
+    }
+    
+    @Override
+	public
+    int[] generateScedule() {
+        // TODO Auto-generated method stub
+        int[] schedule = new int[10];
+        for(int i = 0; i < schedule.length; i++)
+            schedule[i] = TRANSMIT_N_LISTEN;
+        return schedule;
+    }
+
+	@Override
+	public int scheduleLength() {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+}
