6388a09c89ea505ec26dfa57218c4a8d02ca4906
carlson5uiuc <carlson5uiuc@a0ffda31-7524-4b21-e595-ff3db66bd2c9>
2010-11-18 06:55:26 +0000

    git-svn-id: https://boomshakalaka.googlecode.com/svn/trunk@75 a0ffda31-7524-4b21-e595-ff3db66bd2c9

--- CS598rhk/src/edu/illinois/CS598rhk/services/PowerManagement.java
+++ CS598rhk/src/edu/illinois/CS598rhk/services/PowerManagement.java
@@ -9,8 +9,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Date;
 
-import android.os.BatteryManager; 
+import android.os.BatteryManager;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
@@ -20,74 +21,73 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 
-
 public class PowerManagement extends Service implements Runnable {
-    private static final String POWER_TAG = "AdHocClient_power";
-    private static final String LOG_MESSAGE_TAG = "AdHocClient_logMessage";
-    
-    private BatteryInfoReceiver myBatteryInfoReceiver;
-    private BatteryInfoReceiver myBatteryInfoReceiver2;
-    
-    public static final String ACTION_LOG_UPDATE = "log update";
+	private static final String POWER_TAG = "AdHocClient_power";
+	private static final String LOG_MESSAGE_TAG = "AdHocClient_logMessage";
+
+	private BatteryInfoReceiver myBatteryInfoReceiver;
+	private BatteryInfoReceiver myBatteryInfoReceiver2;
+	private long startTime;
+
+	public static final String ACTION_LOG_UPDATE = "log update";
 	public static final String LOG_MESSAGE = "log message";
-	
-    String filename = "power_log";
-    FileOutputStream fos;
-    
-    
-    @Override
-    public void onCreate() {
-    	//TODO implement the function
-    }
-    
-    
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-    	//File outputFile = new File("data/data/edu.illinois.CS598rhk/files", filename);
-        //if (outputFile.exists()) {
-        //	deleteFile(filename);
-        //}
-    	
-    	long curTime = SystemClock.elapsedRealtime();
-    	filename = filename + "." + curTime;
-    	
-        IntentFilter batteryFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        batteryFilter.addAction(POWER_SERVICE);
-        myBatteryInfoReceiver = new BatteryInfoReceiver();
-        registerReceiver(myBatteryInfoReceiver, batteryFilter);
-        
-        IntentFilter logFilter = new IntentFilter(ACTION_LOG_UPDATE);
-        batteryFilter.addAction(LOG_MESSAGE);
-        myBatteryInfoReceiver2 = new BatteryInfoReceiver();
-        registerReceiver(myBatteryInfoReceiver2, logFilter);
-        return START_STICKY;
-    }
-    
-    
-    private class BatteryInfoReceiver extends BroadcastReceiver {
-        
-        public void onReceive(Context context, Intent intent) {
-        	if(intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED))
-        	{
-	            int rawlevel = intent.getIntExtra("level", -1);
-	            int scale = intent.getIntExtra("scale", -1);
-	            int status = intent.getIntExtra("status", -1);
-	            int health = intent.getIntExtra("health", -1);
-	            int plugged = intent.getIntExtra("plugged", -1);
-	            int voltage = intent.getIntExtra("voltage", -1);
-	            int level = -1;
-	            if (rawlevel >= 0 && scale > 0) {
-	                level = (rawlevel * 100) / scale;
-	            }
-	            
-	            long curTime = SystemClock.elapsedRealtime();
-	            String log_output = "Time: " + curTime + "; Battery: level = " + level 
-        						+ ", scale = " + scale + ", status = " + status + 
-        						", health = " + health + ", plugged = " + plugged + 
-        						", voltage = " + voltage + "\n";
-	            
-	            Log.d(POWER_TAG, log_output);
-				
+
+	String filename = "power_log";
+	FileOutputStream fos;
+
+	@Override
+	public void onCreate() {
+		// TODO implement the function
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// File outputFile = new File("data/data/edu.illinois.CS598rhk/files",
+		// filename);
+		// if (outputFile.exists()) {
+		// deleteFile(filename);
+		// }
+
+		startTime = SystemClock.elapsedRealtime();
+		filename = filename + "." + startTime;
+
+		IntentFilter batteryFilter = new IntentFilter(
+				Intent.ACTION_BATTERY_CHANGED);
+		batteryFilter.addAction(POWER_SERVICE);
+		myBatteryInfoReceiver = new BatteryInfoReceiver();
+		registerReceiver(myBatteryInfoReceiver, batteryFilter);
+
+		IntentFilter logFilter = new IntentFilter(ACTION_LOG_UPDATE);
+		batteryFilter.addAction(LOG_MESSAGE);
+		myBatteryInfoReceiver2 = new BatteryInfoReceiver();
+		registerReceiver(myBatteryInfoReceiver2, logFilter);
+
+		return START_STICKY;
+	}
+
+	private class BatteryInfoReceiver extends BroadcastReceiver {
+
+		public void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+				int rawlevel = intent.getIntExtra("level", -1);
+				int scale = intent.getIntExtra("scale", -1);
+				int status = intent.getIntExtra("status", -1);
+				int health = intent.getIntExtra("health", -1);
+				int plugged = intent.getIntExtra("plugged", -1);
+				int voltage = intent.getIntExtra("voltage", -1);
+				int level = -1;
+				if (rawlevel >= 0 && scale > 0) {
+					level = (rawlevel * 100) / scale;
+				}
+
+				long curTime = SystemClock.elapsedRealtime();
+				String log_output = "Time: " + curTime + "; Battery: level = "
+						+ level + ", scale = " + scale + ", status = " + status
+						+ ", health = " + health + ", plugged = " + plugged
+						+ ", voltage = " + voltage + "\n";
+
+				Log.d(POWER_TAG, log_output);
+
 				try {
 					fos = openFileOutput(filename, Context.MODE_APPEND);
 					fos.write(log_output.getBytes());
@@ -99,17 +99,18 @@ public class PowerManagement extends Service implements Runnable {
 					Log.e(POWER_TAG, "Log file could not be written to");
 					e.printStackTrace();
 				}
-        	}
-        	
-        	else if (intent.getAction().equals(ACTION_LOG_UPDATE))
-        	{
-        		String message = intent.getStringExtra(LOG_MESSAGE);
-        		long curTime = SystemClock.elapsedRealtime();
-        		
-        		String log_output = "Time: " + curTime + "; " + message + "\n";
-        		
-        		Log.d(LOG_MESSAGE_TAG, log_output);
-				
+			}
+
+			else if (intent.getAction().equals(ACTION_LOG_UPDATE)) {
+				String message = intent.getStringExtra(LOG_MESSAGE);
+				long curTime = SystemClock.elapsedRealtime();
+				Date now = new Date(curTime - startTime);
+				String log_output = "Time: " + now.getHours() + ":"
+						+ now.getMinutes() + ":" + now.getSeconds() + "; "
+						+ message + "\n";
+
+				Log.d(LOG_MESSAGE_TAG, log_output);
+
 				try {
 					fos = openFileOutput(filename, Context.MODE_APPEND);
 					fos.write(log_output.getBytes());
@@ -121,21 +122,20 @@ public class PowerManagement extends Service implements Runnable {
 					Log.e(LOG_MESSAGE_TAG, "Log file could not be written to");
 					e.printStackTrace();
 				}
-        	}
-        }
-    }
-    
-    
-    @Override
-    public IBinder onBind(Intent arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        
-    }
+			}
+		}
+	}
 
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
