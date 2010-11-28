1333f13b906cdf928496a5078bfb4c45dcb96fb7
carlson5uiuc <carlson5uiuc@a0ffda31-7524-4b21-e595-ff3db66bd2c9>
2010-11-28 03:14:36 +0000

    git-svn-id: https://boomshakalaka.googlecode.com/svn/trunk@88 a0ffda31-7524-4b21-e595-ff3db66bd2c9

--- CS598rhk/res/raw/load.sh
+++ CS598rhk/res/raw/load.sh
@@ -6,6 +6,7 @@ insmod /data/data/edu.illinois.CS598rhk/bin/mac80211.ko
 insmod /data/data/edu.illinois.CS598rhk/bin/wl1251.ko
 insmod /data/data/edu.illinois.CS598rhk/bin/wl1251_sdio.ko
 insmod /data/data/edu.illinois.CS598rhk/bin/msm_wifi.ko
+sleep 5
 iw dev wlan0 set power_save off
 iw dev wlan0 set type ibss
 
