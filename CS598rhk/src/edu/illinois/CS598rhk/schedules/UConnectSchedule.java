6962058151678a66ffeb57737a1a4845f2178980
carlson5uiuc <carlson5uiuc@a0ffda31-7524-4b21-e595-ff3db66bd2c9>
2011-03-01 18:28:59 +0000

    added uconnect schedule class
    git-svn-id: https://boomshakalaka.googlecode.com/svn/trunk@197 a0ffda31-7524-4b21-e595-ff3db66bd2c9

---
+++ CS598rhk/src/edu/illinois/CS598rhk/schedules/UConnectSchedule.java
@@ -0,0 +1,51 @@
+package edu.illinois.CS598rhk.schedules;
+
+import java.util.Random;
+
+
+public class UConnectSchedule extends DiscoverSchedule {
+
+    private int prime;
+    
+    public UConnectSchedule(int period) {
+        this.prime = period;
+    }
+    
+    public void setPeriod(int period)
+    {
+        this.prime = period;
+    }
+    
+    //TODO: Make it so Uconnect is Uconnect and not searchlight
+    @Override
+	public int[] generateScedule() {
+        int[] schedule = new int[this.prime * (this.prime / 2)];
+        
+        for (int i=0; i<schedule.length; ++i) {
+            if (i % this.prime == 0) {
+                schedule[i] = TRANSMIT_N_LISTEN;
+            }
+            else {
+                schedule[i] = DO_NOTHING;
+            }
+        }
+        
+        Random rand = new Random();
+        for (int i=0; i<(this.prime / 2); ++i) {
+            int poll = (rand.nextInt(this.prime-1) + 1) + (i * this.prime);
+            schedule[poll] = TRANSMIT_N_LISTEN;
+        }
+        
+        return schedule;
+    }
+
+	public int scheduleLength() {
+		return (int) Math.ceil(prime*prime/2);
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Searchlight Schedule " + " t=" + prime;
+	}
+}
